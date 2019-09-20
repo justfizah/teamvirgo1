6c9794684f85f7cc6c496a0004299dcd3b0ee263
Hafizah Muhyideen <40233874+justfizah@users.noreply.github.com>
2019-09-20 20:35:50 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -6,11 +6,11 @@ session_start();
 
 
 
-$con = mysqli_connect('localhost','root','');
+$con = mysqli_connect('s9xpbd61ok2i7drv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','root','');
 
 
 
-mysqli_select_db($con, 'userregistration');
+mysqli_select_db($con, 'bayyw9cgyjrq29a8');
 
 
 $username = $_POST['username'];
@@ -18,7 +18,7 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 
 
-$s = " select * from usertable where username = '$username'";
+$s = " select * from db0bulyd29gens6a where username = '$username'";
 
 $result = mysqli_query($con, $s);
 
@@ -28,8 +28,10 @@ $num = mysqli_num_rows($result);
 if ($num == 1) {
 	echo "Username Already Taken";
 }else{
-	$reg= "insert into usertable(username , email , password ) values ('$username' ,  '$email', '$password')";
+	$reg= "insert into db0bulyd29gens6a(username , email , password) values ('$username' ,  '$email', '$password')";
 	mysqli_query($con, $reg);
-	echo "Registration Successful";}
+
+	echo "Registration Successful";
+}
 
  ?>
