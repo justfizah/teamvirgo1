e99b58d0897ff72da4c46a5ca25cdcfbf78d25c1
silas adedoyin <silasfaarda@gmail.com>
2019-09-20 12:54:47 -1200

    responsive design

---
+++ new/js/script.js
@@ -0,0 +1,58 @@
+const signUpForm = document.getElementById("reg-form");
+const loginForm = document.getElementById("login-form");
+const regHeading = document.getElementById("reg-heading");
+const loginHeading = document.getElementById("login-heading");
+const showRegBtn = document.getElementById("show-reg-btn");
+const showLoginBtn = document.getElementById("show-login-btn");
+const showRegLink = document.getElementById("show-reg-link");
+const showLoginLink = document.getElementById("show-login-link");
+const ctaLogin = document.getElementById("cta-login");
+const ctaReg = document.getElementById("cta-reg");
+
+showLoginBtn.addEventListener('click', showLogin, true);
+showLoginLink.addEventListener('click', showLogin, true);
+showRegBtn.addEventListener('click', showReg, true);
+showRegLink.addEventListener('click', showReg, true);
+
+
+// shows the login form
+function showLogin(e){
+    
+    if(typeof e != 'undefined'){
+        e.preventDefault();
+    }
+
+    hideEl(signUpForm);
+    hideEl(regHeading);
+    hideEl(ctaReg);
+    hideEl(showLoginLink);
+    showEl(loginForm);
+    showEl(loginHeading);
+    showEl(ctaLogin);
+    showEl(showRegLink);
+}
+
+// shows the registration form
+function showReg(e){
+
+    e.preventDefault();
+
+    hideEl(loginForm);
+    hideEl(loginHeading);
+    hideEl(ctaLogin);
+    hideEl(showRegLink);
+    showEl(signUpForm);
+    showEl(regHeading);
+    showEl(ctaReg);
+    showEl(showLoginLink);
+}
+
+function showEl(el){
+    el.style.display = "block";
+}
+
+function hideEl(el){
+    el.style.display = "none";
+}
+
+showLogin();
